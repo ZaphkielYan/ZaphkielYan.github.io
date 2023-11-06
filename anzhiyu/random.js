566c06baea66f644d3eb6fbf836f3d3975ecd2b7
zaphkielyan <2676311890@qq.com>
2023-11-06 23:18:21 +0800

    Site updated: 2023-11-06 23:18:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/07/30/One-NewBook/","2023/08/08/The-Cook/","2023/09/10/University-1/","2023/09/24/University-2/","2023/10/16/University-3/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/07/30/One-NewBook/","2023/08/08/The-Cook/","2023/09/10/University-1/","2023/09/24/University-2/","2023/11/06/University-4/","2023/10/16/University-3/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
